f4965acb4a99b5c2c2850f7e3399e3284fb430ae
Siddiqur Rahman <infobusiness.anay@gmail.com>
2025-09-16 21:09:36 +0600

    Add new files

---
+++ datatype.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    // An integer variable num that will contain 10, c language that prints an integer number
+    int m = 10;
+    printf("Number is %d\n", m);
+
+    int num1 = 20, num2 = 30;
+    printf("Number is %d %d\n", num1, num2);
+
+    // Write a program that prints a floating, double and character
+
+    float one = 10.5;
+    double two = 10.525555554;
+    char ch = 'a';
+
+    printf("one = %f\n", one);
+    printf("two = %lf\n", two);
+    printf("ch = %c\n", ch);
+    
+    return 0;
+}
---
+++ scanfinput.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    // Write a program that takes an integer and print that number
+
+    int num;
+    printf("Please enter an integer =");
+    scanf("%d", &num);
+    printf("You have pressed = %d\n", num);
+
+    return 0;
+}
