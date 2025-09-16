82bf38075179df13898f5d0de242152ccb6c3266
Siddiqur Rahman <infobusiness.anay@gmail.com>
2025-09-16 12:01:03 +0600

    escape sequence file

---
+++ escape_sequence.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    // c language escape sequence
+    
+    printf("Mango is taste\a\n");
+    printf("How \bare you?\n");
+    printf("C is\f the mother of programminglanguage\n");
+    printf("This is new line\n");
+    printf("What's \rup\n");
+    printf("Do you\t play Football?\n");
+    printf("Do you\v play Cricket?\n");
+    printf("Let's go to \0School\n");
+    printf("\'Hardwork beats talent\'\n");
+    printf("\"Don't Give up\"\n");
+    printf("This is \\backslash\n");
+    printf("Did you wanna do Volunteering\?");
+
+    return 0;
+}
