43a1ca118636fc31f177ac9879b638632a62ce3c
Siddiqur Rahman <infobusiness.anay@gmail.com>
2025-09-17 16:03:18 +0600

    Add new file

---
+++ format_specifiers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    // Commonly Used Format Specifiers
+    
+    %c: Character
+    %s: String (array of characters)
+    %d: or %i: Signed integer (decimal)
+    %u: Unsigned integer (decimal)
+    %f: Float (decimal floating-point)
+    %lf: Double (decimal floating-point)
+    %Lf: Long double (decimal floating-point)
+    %e: or %E: Scientific notation for floats
+    %o: Unsigned octal integer
+    %x: or %X: Unsigned hexadecimal integer
+    %p: Pointer address
+    %%: Prints a literal '%' character
+
+    return 0;
+}
